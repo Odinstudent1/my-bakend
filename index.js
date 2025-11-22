1345115e9b49192ab279141e18a81543411487a2
Odinstudent1 <parthupmanyuu@gmail.com>
2025-11-23 00:09:18 +0530

    Integrate PostgreSQL and add /db-test route
    Added database connection and test route for PostgreSQL.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { Pool } = require("pg");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+
+// connect to database
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+// test route
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ time: result.rows[0] });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is working!" });
@@ -13,3 +34,4 @@ const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
+
